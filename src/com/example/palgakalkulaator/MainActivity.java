6f5040148639e577f44adfb6a5e052b74822a175
Indrek Kõue <indrek.koue@gmail.com>
2013-01-06 22:13:31 +0200

    first com

---
+++ src/com/example/palgakalkulaator/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.palgakalkulaator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		EditText et = (EditText) findViewById(R.id.EditText06);
+		final EditText et2 = (EditText) findViewById(R.id.EditText05);
+		final EditText et3 = (EditText) findViewById(R.id.EditText04);
+
+				
+		et.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				et2.setText(Integer.toString(Integer.parseInt(s.toString()) * 10));
+				et3.setText(Integer.toString(Integer.parseInt(s.toString()) * 33));
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
